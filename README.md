0b6e646c1f7f306d22a0eeb3d33b11c078526c9f
Bulat <65621634+David2261@users.noreply.github.com>
2023-05-29 22:15:01 +0300

    Add Makefile and ReadMe for ping pong (#4)
    * Add mandelbrot
    
    * Add Makefile and ReadMe for ping pong

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Ping_Pong_C
+# Games_C
+
+## Games
+    - Game of Life
+    - Ping Pong
+    - Math calculations (not a game)
+
+## Start
+```bash
+make
+```
+./{your_game}
+
+
+
+
--- det.c
+++ det.c
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 void input_dimens(int *cnt1, int *cnt2);
 void input_matrix(double **matrix, int const *cnt1, int const *cnt2);
 void sub_matr(int n, double M[n-1][n-1], double m[n][n], int I, int J);
 double determinant(int n, double m[n][n]);
 void exit_print_na();
 void output(double **matrix, int *cnt);
+
 
 int main() {
     int count1, count2;
@@ -18,6 +20,8 @@ int main() {
     output(matr,  &count1);
     return 0;
 }
+
+// Ввод размера матрицы
 void input_dimens(int *cnt1, int *cnt2) {
     int dim1, dim2;
     if (scanf("%d %d", &dim1, &dim2) == 2) {
@@ -31,12 +35,14 @@ void input_dimens(int *cnt1, int *cnt2) {
         exit_print_na();
     }
 }
+
+// Ввод значения матрицы
 void input_matrix(double **matrix, int const *cnt1, int const *cnt2) {
     double tm;
     for (int i = 0; i < *cnt1; i++) {
         matrix[i] = malloc(*cnt2 * sizeof(double));
     }
-      for (int i = 0; i < *cnt1; i++) {
+      for (int i = 0; i < *cnt1; i++)
         for (int j = 0; j < *cnt2; j++) {
             if (scanf("%lf", &tm) == 1) {
                 matrix[i][j] = tm;
@@ -44,58 +50,56 @@ void input_matrix(double **matrix, int const *cnt1, int const *cnt2) {
                 exit_print_na();
             }
         }
-      }
 }
 
+// Выход из системы
 void exit_print_na() {
     printf("%s", "n/a");
     exit(1);
 }
 
+// Определитель
 double determinant(int n, double m[n][n]) {
-      double det = 0;
-      if (n == 1) {
-          return m[0][0];
+    double det = 0;
+    if (n == 1) return m[0][0];
+
+    if (n == 2) {
+        det = m[0][0] * m[1][1] - m[0][1] * m[1][0];
+    } else {
+        double M[n - 1][n - 1];
+        for (int i = 0; i < n; i++) {
+            sub_matr(n, M, m, 0, i);
+            det += m[0][i] * pow(-1, i) * determinant(n - 1, M);
         }
-      if (n == 2) {
-          det = m[0][0] * m[1][1] - m[0][1] * m[1][0];
-        } else {
-          double M[n - 1][n - 1];
-          for (int i = 0; i < n; i++) {
-          sub_matr(n, M, m, 0, i);
-          det += m[0][i] * pow(-1, i) * determinant(n - 1, M);
-          }
-        }
-      return det;
     }
+    return det;
+}
+
 
 void sub_matr(int n, double M[n-1][n-1], double m[n][n], int I, int J) {
     int i, a = 0, b = 0;
     int j;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
         if (i != I) {
-            for (j = 0; j < n; j++) {
+            for (j = 0; j < n; j++)
                 if (J != j) {
                     M[a][b] = m[i][j];
                     b++;
                 }
-            }
             a++;
             b = 0;
         }
-    }
 }
 
+// Вывод
 void output(double **matrix, int *cnt) {
     double m[*cnt][*cnt];
-    for (int i = 0; i < *cnt; i++) {
-      for (int j = 0; j < *cnt; j++) {
+    for (int i = 0; i < *cnt; i++)
+      for (int j = 0; j < *cnt; j++)
           m[i][j] = matrix[i][j];
-      }
-    }
-    for (int i = 0; i < *cnt; i++) {
-        free(matrix[i]);
-    }
+
+    for (int i = 0; i < *cnt; i++) free(matrix[i]);
+
     free(matrix);
     printf("%f", determinant(*cnt, m));
 }
